8b3f787db546f26d66fce430e64f1711b4f44fbb
iumehara <iumehara10@gmail.com>
2013-03-28 22:49:59 -0400

    created march28

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'pg'
+
+group :development, :test do
+	gem 'rspec-rails', '2.11.0'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :test do
+	gem 'capybara', '1.1.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+March28::Application.config.secret_token = 'd98eb17ae87e9474ba5799303f60c2c4551f3c9ba27e10b3bfba48181ce0479c80d8f45a3c88faec057138dd7fad1d3eb132fdcd994e45f09c4610efaced9841'
